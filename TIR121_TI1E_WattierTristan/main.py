132ecced43822bcab3b01279e2f030d7a6e2796e
Tristan W <Sigilaire@protonmail.com>
2022-11-14 18:20:36 +0100

    get_host passe le test

--- TIR121_TI1E_WattierTristan/main.py
+++ TIR121_TI1E_WattierTristan/main.py
@@ -1,6 +1,7 @@
 from file_read import lines_from_file
-from getters import get_host, get_complete_date
+from getters import get_host, get_complete_date, get_message
 
 file = lines_from_file(r"TIR121_TI1E_WattierTristan\log\syslog.log")
 
 print(get_host(file[0]))
+print(get_complete_date(file[0]))
