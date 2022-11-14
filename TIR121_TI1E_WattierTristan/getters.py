579c235ca5c95aa617328d3b8c6b9d72e361be55
Tristan W <Sigilaire@protonmail.com>
2022-11-14 19:43:55 +0100

    Tous les get_messages passent le test (4)

--- TIR121_TI1E_WattierTristan/getters.py
+++ TIR121_TI1E_WattierTristan/getters.py
@@ -19,6 +19,18 @@ def get_message(line):
             !!! le message peut être composé de sous messages (séparés pas d’autres « : »),
                 dans ce cas, il faut tout
     """
-    if ":" in line:
-        split_line = line.split(":")
-        return ":".join(split_line[3:])
+    split_line = line.split(":")
+    return ":".join(split_line[3:])
+    
+def get_program(line):
+    """ Pre : line est une ligne de log bien formée (str)
+        Post : Retourne le nom du programme
+    """
+    split_line = line.split()
+    return split_line[4]
+    
+def get_process_id(line):
+    """ Pre : line est une ligne de log bien formée (str)
+        Post : Retourne le numéro du processus. Si aucun id n’est disponible
+            (dans le cas d’un kernel par exemple), -1 est retourné.
+    """
--- TIR121_TI1E_WattierTristan/main.py
+++ TIR121_TI1E_WattierTristan/main.py
@@ -1,8 +1,16 @@
 from file_read import lines_from_file
-from getters import get_host, get_complete_date, get_message
+from getters import get_host, get_complete_date, get_message, get_program
 
 file = lines_from_file(r"TIR121_TI1E_WattierTristan\log\syslog.log")
 
+# for i in range(50):
+#     print(f"\n{i+1}\n")
+#     print(f"{get_host(file[i])}")
+#     print(f"{get_complete_date(file[i])}")
+#     print(f"{get_message(file[i])}")
+#     print(f"{get_program(file[i])}")
+
 print(get_host(file[0]))
 print(get_complete_date(file[0]))
-print(get_message(file[2]).strip("[ ]"))
+print(get_message(file[0]))
+print(get_program(file[0]))
