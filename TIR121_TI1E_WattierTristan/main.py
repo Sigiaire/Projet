90c34ed3773de5a4e46a82e4d945417500638e5d
Tristan W <Sigilaire@protonmail.com>
2022-12-06 15:18:46 +0100

    5 failed

--- TIR121_TI1E_WattierTristan/main.py
+++ TIR121_TI1E_WattierTristan/main.py
@@ -5,21 +5,74 @@ from dates import logs_by_day, formated_date, logs_between
 # from getters import get_host, get_complete_date, get_message, get_program, get_process_id
 # from dates import logs_by_day, formated_date, logs_between
 
+def logs_with_tag(logs, tag="error"):
+    """ Pre :
+        - logs est une liste où chaque élément est une ligne de log bien formée
+        - tag est une chaine de caractères à trouver dans le message.
+            Par défaut : "error"
+    Post :
+        - retourne une liste de logs qui concernent uniquement des logs
+            contenant le tag (minuscule ou majuscule) dans le message
+    """
+    error_logs = []
+    
+    for log_line in logs:
+        if tag in log_line.lower():
+            error_logs.append(log_line)
+            
+    return error_logs
+
+def logs_from_program(logs, program):
+    """ Pre :
+        - logs est une liste où chaque élément est une ligne de log bien formée
+        - program (str) est le programme à trouver
+    Post :
+        - retourne une liste de logs qui concernent uniquement les programmes
+            correspondant à "program"
+    """
+    error_logs = []
+    
+    for log_line in logs:
+        if program in log_line:
+            error_logs.append(log_line)
+            
+    return error_logs
+
+def list_process_for_program(logs, program):
+    """ Pre :
+        - logs est une liste où chaque élément est une ligne de log bien formée
+        - program (str) est le programme à trouver
+    Post :
+        - retourne une liste des process_id gérés par le programme.
+            La liste ne contient aucun doublon.
+"""
+    processes = []
+    
+    for log_line in logs:
+        log_program = get_program(log_line)
+        if program in log_program:
+            process = get_process_id(log_line)
+            processes.append(process)
+            
+    return processes
+
 if __name__ == '__main__':
     file = lines_from_file(r"TIR121_TI1E_WattierTristan\log\syslog.log")
     
+    # print(get_host(file[1]))
+    # print(get_complete_date(file[1]))
+    # print(get_message(file[1]))
+    # print(get_program(file[1]))
+    # print(get_process_id(file[1]))
     
+    # print(logs_by_day(file, "Oct 26"))
+    # print(formated_date("Oct 26 00:00:00"))
+    # print(logs_between(file, "Oct 26 00:00:00"))
     
+    # print(logs_with_tag(file))
+    # print(logs_from_program(file, "kernel"))
     
-    print(get_host(file[1]))
-    print(get_complete_date(file[1]))
-    print(get_message(file[1]))
-    print(get_program(file[1]))
-    print(get_process_id(file[1]))
-    
-    print(logs_by_day(file, "Oct 26"))
-    print(formated_date("Oct 26 00:00:00"))
-    print(logs_between(file, "Oct 26 00:00:00"))
+    print(list_process_for_program(file, "kernel"))
     
 # for i in range(5000):
 #     print(f"\n=============================\n======LIGNE NUMÉRO {i+1}======\n=============================")
@@ -28,3 +81,5 @@ if __name__ == '__main__':
 #     print(f"===MESSAGE===  {get_message(file[i])}")
 #     print(f"===PROGRAMME===  {get_program(file[i])}")
 #     print(f"===PROCESS_ID===  {get_process_id(file[i])}")
+#     print(f"===DATE_FORMATÉE===  {formated_date(get_complete_date(file[i]))}")
+    
--- TIR121_TI1E_WattierTristan/test_main.py
+++ TIR121_TI1E_WattierTristan/test_main.py
@@ -71,7 +71,7 @@ def test_formated_date_december():
     """ Test unitaire pour formated_date """
     assert main.formated_date("Dec 18 22:30:15") == "12:18 22:30:15"
 
-L_TEST = [
+L_TEST_DATE = [
         "Jan 13 22:30:15 blabla",
         "Jan 14 23:30:15 blabla",
         "Jan 16 26:30:15 blabla",
@@ -80,7 +80,7 @@ L_TEST = [
     ]
 def test_logs_by_day1():
     """ Test unitaire pour logs_by_day """
-    output = main.logs_by_day(L_TEST, "Jan 14")
+    output = main.logs_by_day(L_TEST_DATE, "Jan 14")
     expected = [
         "Jan 14 23:30:15 blabla",
         "Jan 14 24:30:15 blabla"
@@ -89,18 +89,17 @@ def test_logs_by_day1():
         assert elem in expected
     for elem in expected :
         assert elem in output
-    
 
 def test_logs_by_day2():
     """ Test unitaire pour logs_by_day """
-    output = main.logs_by_day(L_TEST, "Jan 13")
+    output = main.logs_by_day(L_TEST_DATE, "Jan 13")
     expected = ["Jan 13 22:30:15 blabla"]
     assert output == expected
 
 
 def test_logs_between():
     """ Test unitaire pour logs_between """
-    output = main.logs_between(L_TEST, "01:14 24:29:15", "01:16 26:31:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:29:15", "01:16 26:31:15")
     expected = [
         "Jan 14 24:30:15 blabla",
         "Jan 15 25:30:15 blabla",
@@ -113,7 +112,7 @@ def test_logs_between():
 
 def test_logs_between_limite_min():
     """ Test unitaire pour logs_between (min limite) """
-    output = main.logs_between(L_TEST, "01:14 24:30:15", "01:16 26:29:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:30:15", "01:16 26:29:15")
     expected = [
         "Jan 14 24:30:15 blabla",
         "Jan 15 25:30:15 blabla"
@@ -125,7 +124,7 @@ def test_logs_between_limite_min():
 
 def test_logs_between_limite_max():
     """ Test unitaire pour logs_between (max limite) """
-    output = main.logs_between(L_TEST, "01:14 24:29:15", "01:16 26:30:15")
+    output = main.logs_between(L_TEST_DATE, "01:14 24:29:15", "01:16 26:30:15")
     expected = [
         "Jan 14 24:30:15 blabla",
         "Jan 15 25:30:15 blabla",
@@ -138,7 +137,7 @@ def test_logs_between_limite_max():
 
 def test_logs_between_default_min():
     """ Test unitaire pour logs_between (min par défaut) """
-    output = main.logs_between(L_TEST, date_max="01:14 23:40:00")
+    output = main.logs_between(L_TEST_DATE, date_max="01:14 23:40:00")
     expected = [
             "Jan 13 22:30:15 blabla",
             "Jan 14 23:30:15 blabla"
@@ -150,12 +149,150 @@ def test_logs_between_default_min():
 
 def test_logs_between_default_max():
     """ Test unitaire pour logs_between (max par défaut) """
-    output = main.logs_between(L_TEST, date_min="01:14 23:40:00")
+    output = main.logs_between(L_TEST_DATE, date_min="01:14 23:40:00")
     expected = [
         "Jan 16 26:30:15 blabla",
         "Jan 14 24:30:15 blabla",
         "Jan 15 25:30:15 blabla"
     ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+# tests parties 3 :
+L_TEST_3 = [
+        "Oct JJ HH:MM:SS host programA[1]: blabla",
+        "Nov JJ HH:MM:SS host programA[2]: bla: bla: Error404",
+        "Jan JJ HH:MM:SS host programA[2]: oups...",
+        "Feb JJ HH:MM:SS host programB[5]: FATAL ERRoR",
+        "Mar JJ HH:MM:SS host program: message",
+        "Mar JJ HH:MM:SS host program: olala eRRoR",
+        "Dec JJ HH:MM:SS host programB[5]: message d'error",
+        "Apr JJ HH:MM:SS host programA[42]: J'aime les fruits au sirop",
+        "May JJ HH:MM:SS host programA[42]: La progra c'est génial ! Youpie !",
+        "Jun JJ HH:MM:SS host programA[42]: MeSsAgE",
+        "Jul JJ HH:MM:SS host programC[7]: message important : Coucou les copains !",
+        "Aug JJ HH:MM:SS host programC[1]: hello host :)",
+        "Sept JJ HH:MM:SS host program: encore une error"
+    ]
+
+# logs_with_tag
+def test_logs_with_default_tag():
+    """ Test unitaire pour logs_with_tag avec valeur par défaut (error) """
+    output = main.logs_with_tag(L_TEST_3)
+    expected = [
+        "Nov JJ HH:MM:SS host programA[2]: bla: bla: Error404",
+        "Dec JJ HH:MM:SS host programB[5]: message d'error",
+        "Feb JJ HH:MM:SS host programB[5]: FATAL ERRoR",
+        "Sept JJ HH:MM:SS host program: encore une error",
+        "Mar JJ HH:MM:SS host program: olala eRRoR"
+    ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+def test_logs_with_message_tag():
+    """ Test unitaire pour logs_with_tag en cherchant "Message" """
+    output = main.logs_with_tag(L_TEST_3, "Message")
+    expected = [
+        "Dec JJ HH:MM:SS host programB[5]: message d'error",
+        "Mar JJ HH:MM:SS host program: message",
+        "Jun JJ HH:MM:SS host programA[42]: MeSsAgE",
+        "Jul JJ HH:MM:SS host programC[7]: message important : Coucou les copains !"
+    ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+def test_logs_with_empty_tag():
+    """ Test unitaire pour logs_with_tag en cherchant un tag vide """
+    output = main.logs_with_tag(L_TEST_3, "")
+    expected = [
+        "Oct JJ HH:MM:SS host programA[1]: blabla",
+        "Nov JJ HH:MM:SS host programA[2]: bla: bla: Error404",
+        "Dec JJ HH:MM:SS host programB[5]: message d'error",
+        "Jan JJ HH:MM:SS host programA[2]: oups...",
+        "Feb JJ HH:MM:SS host programB[5]: FATAL ERRoR",
+        "Mar JJ HH:MM:SS host program: message",
+        "Mar JJ HH:MM:SS host program: olala eRRoR",
+        "Apr JJ HH:MM:SS host programA[42]: J'aime les fruits au sirop",
+        "May JJ HH:MM:SS host programA[42]: La progra c'est génial ! Youpie !",
+        "Jun JJ HH:MM:SS host programA[42]: MeSsAgE",
+        "Jul JJ HH:MM:SS host programC[7]: message important : Coucou les copains !",
+        "Aug JJ HH:MM:SS host programC[1]: hello host :)",
+        "Sept JJ HH:MM:SS host program: encore une error"
+    ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+def test_logs_with_tag_doesnt_exist():
+    """ Test unitaire pour logs_with_tag qui n'existe pas"""
+    assert main.logs_with_tag(L_TEST_3, "Bob") == []
+
+def test_logs_with_host_tag():
+    """ Test unitaire pour logs_with_tag avec le tag "host" """
+    assert main.logs_with_tag(L_TEST_3, "host") == [
+        "Aug JJ HH:MM:SS host programC[1]: hello host :)"
+    ]
+
+# logs_from_program
+def test_logs_from_program_with_processid():
+    """ Test unitaire pour logs_from_program (où on cherche un programme avec processid) """
+    output = main.logs_from_program(L_TEST_3,"programA")
+    expected = [
+        "Oct JJ HH:MM:SS host programA[1]: blabla",
+        "Nov JJ HH:MM:SS host programA[2]: bla: bla: Error404",
+        "Jan JJ HH:MM:SS host programA[2]: oups...",
+        "Apr JJ HH:MM:SS host programA[42]: J'aime les fruits au sirop",
+        "May JJ HH:MM:SS host programA[42]: La progra c'est génial ! Youpie !",
+        "Jun JJ HH:MM:SS host programA[42]: MeSsAgE"
+    ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+def test_logs_from_program_without_processid():
+    """ Test unitaire pour logs_from_program (où on cherche un programme sans processid) """
+    output = main.logs_from_program(L_TEST_3, "program")
+    expected = [
+        "Mar JJ HH:MM:SS host program: message",
+        "Mar JJ HH:MM:SS host program: olala eRRoR",
+        "Sept JJ HH:MM:SS host program: encore une error",
+    ]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+# list_process_for_program
+def test_list_process_for_program_with_processid():
+    """ Test unitaire pour list_process_for_program (où on cherche un programme avec processid) """
+    output = main.list_process_for_program(L_TEST_3,"programA")
+    expected = [1,2,42]
+    for elem in output :
+        assert elem in expected
+    for elem in expected :
+        assert elem in output
+
+def test_list_process_for_program_without_processid():
+    """ Test unitaire pour list_process_for_program (où on cherche un programme sans processid) """
+    assert main.list_process_for_program(L_TEST_3,"program") == []
+
+#Alert si un programme génère plus de x erreurs sur une journée
+
+
+# suspects
+def test_suspects():
+    """ Test unitaire pour suspects """
+    output = main.suspects(L_TEST_3,1)
+    expected = ["programB", "program"]
+    assert len(output) == 2
     for elem in output :
         assert elem in expected
     for elem in expected :
